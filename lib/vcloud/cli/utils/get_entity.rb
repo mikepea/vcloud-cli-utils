0e84801c48eeb7be88443632443e808fb57d9d17
Mike Pountney <Mike.Pountney@gmail.com>
2014-03-19 11:12:49 +0000

    fix vcloud-get-entity invocation

--- lib/vcloud/cli/utils/get_entity.rb
+++ lib/vcloud/cli/utils/get_entity.rb
@@ -6,34 +6,39 @@ module Vcloud
   module CLI
     module Utils
       class GetEntity
+
         include Methadone::Main
         include Methadone::CLILogging
 
-        main do |entity_type, identifier|
-          vcloud = ::Fog::Compute::VcloudDirector.new
-          case entity_type
-          when 'vm'
-            pp vcloud.get_vapp(identifier).body
-          when 'vApp'
-            pp vcloud.get_vapp(identifier).body
-          when 'edgeGateway'
-            pp vcloud.get_edge_gateway(identifier).body
-          when 'orgVdcNetwork'
-            pp vcloud.get_network_complete(identifier).body
-          when 'orgVdc'
-            pp vcloud.get_vdc(identifier).body
-          else
-            pp eval "vcloud.#{entity_type}(identifier).body"
+        def self.run
+
+          main do |entity_type, identifier|
+            vcloud = ::Fog::Compute::VcloudDirector.new
+            case entity_type
+            when 'vm'
+              pp vcloud.get_vapp(identifier).body
+            when 'vApp'
+              pp vcloud.get_vapp(identifier).body
+            when 'edgeGateway'
+              pp vcloud.get_edge_gateway(identifier).body
+            when 'orgVdcNetwork'
+              pp vcloud.get_network_complete(identifier).body
+            when 'orgVdc'
+              pp vcloud.get_vdc(identifier).body
+            else
+              pp eval "vcloud.#{entity_type}(identifier).body"
+            end
           end
+
+          arg :entity_type
+          arg :identifier
+
+          description "
+          "
+
+          go!
+
         end
-
-        arg :entity_type
-        arg :identifier
-
-        description "
-        "
-
-        go!
       end
 
     end
